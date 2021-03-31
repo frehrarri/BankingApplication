2671a4e531b05b6b409f478500b26a74b7d2e1fb
Mitchell <mitchellfrehr@gmail.com>
2021-03-31 12:06:44 -0400

    commented out connection test

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -7,13 +7,13 @@ import com.banking.utils.*;
 public class Driver {
 
 	public static void main(String[] args) {
-
+		
 		//tests our connection
-		try (Connection conn = ConnectionUtil.getConnection()) {
-			System.out.println("Connection Successful");
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
+//		try (Connection conn = ConnectionUtil.getConnection()) {
+//			System.out.println("Connection Successful");
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
 		
 		StartMenu start = new StartMenu();
 		start.startMenu();
