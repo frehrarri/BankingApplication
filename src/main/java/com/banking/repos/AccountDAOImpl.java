02b72cd551f0a054de71bb3f7fe156c4e2f14e9f
Mitchell <mitchellfrehr@gmail.com>
2021-04-04 13:50:24 -0400

    added preparestatements and statements to execute SQL commands

--- src/main/java/com/banking/repos/AccountDAOImpl.java
+++ src/main/java/com/banking/repos/AccountDAOImpl.java
@@ -1,8 +1,11 @@
 package com.banking.repos;
 
 import java.sql.Connection;
+
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import com.banking.models.UserAccounts;
 import com.banking.utils.ConnectionUtil;
@@ -18,17 +21,38 @@ public class AccountDAOImpl implements AccountDAO {
 			
 			PreparedStatement ps = conn.prepareStatement(sql);
 			
-			//ps.setDouble(updateUserAccount.getBalance());
+			int index = 0;
+			ps.setDouble(++index, updateUserAccount.getBalance());
+			ps.setNull(++index, java.sql.Types.INTEGER);
+			ps.setString(++index,  updateUserAccount.getUid());
+			
+			ps.executeQuery(sql);
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	@Override
-	public void findById() {
-		// TODO Auto-generated method stub
+	public void findById(String genId) {
+		
+		try (Connection conn=ConnectionUtil.getConnection()){
+			String sql = "SELECT * FROM account_info WHERE account_id = "+genId+";";
+			
+			Statement statement = conn.createStatement();
+			
+			ResultSet result = statement.executeQuery(sql);
+			
+			if (result.next()) {
+				UserAccounts acc = new UserAccounts();
+				acc.setUid(result.getString("account_id"));
+			}
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
