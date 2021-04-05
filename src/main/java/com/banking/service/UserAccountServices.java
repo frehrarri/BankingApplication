cafea36625eb772a8e9f612e13e7096e7491a37f
Mitchell <mitchellfrehr@gmail.com>
2021-04-05 12:12:01 -0400

    created deposit method

--- src/main/java/com/banking/service/UserAccountServices.java
+++ src/main/java/com/banking/service/UserAccountServices.java
@@ -1,18 +1,39 @@
 package com.banking.service;
 
+import com.banking.models.UserAccounts;
+import com.banking.repos.AccountDAO;
+import com.banking.repos.AccountDAOImpl;
+
 public class UserAccountServices {
 
-	public void deposit() {
-		
-	}
-	
+	AccountDAO aDao = new AccountDAOImpl();
 
+	public UserAccounts getAccountById(int id) {
+		return aDao.findById(id);
+	}
+
+	public boolean deposit(double amount, UserAccounts acc) {
+		if (amount < 1) {
+			return false;
+		}
+
+		acc.setBalance(amount + acc.getBalance());
+		if (aDao.updateAccount(acc)) {
+			return true;
+		}
+		return false;
+	}
+
+	
+	
 	public void withdraw() {
 	}
 
 	
-
+	
 	public void transfer() {
 	}
+
+	
 	
 }
