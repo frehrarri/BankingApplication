2fabe5512c7328c054eef5008015cff616cf254d
Mitchell <mitchellfrehr@gmail.com>
2021-04-05 19:51:05 -0400

    updated database

--- BankAppl.sql
+++ BankAppl.sql
@@ -1,16 +1,19 @@
 CREATE DATABASE user_accounts;
 
-CREATE TABLE account_info(account_id SERIAL, first_name VARCHAR(30), last_name VARCHAR(30),
-	email VARCHAR(30), primary_phone INTEGER
+CREATE TABLE account_info(account_id SERIAL, 
+	first_name VARCHAR(30), 
+	last_name VARCHAR(30),
+	email VARCHAR(30) UNIQUE, 
+	primary_phone INTEGER UNIQUE,
+	bank_id VARCHAR(20) UNIQUE,
+	account_balance NUMERIC (11,2),
+	PRIMARY KEY (account_id)
 );
 
-ALTER TABLE account_info ADD COLUMN account_balance NUMERIC (11,2);
-
-ALTER TABLE account_info PRIMARY KEY (account_id);
 
 CREATE TABLE credentials(
 	id SERIAL,
-	username VARCHAR(15), 
+	username VARCHAR(15) UNIQUE, 
 	userpass VARCHAR(15), 
 	acct_ref_id SERIAL,
 	PRIMARY KEY (id),
@@ -18,9 +21,3 @@ CREATE TABLE credentials(
 		FOREIGN KEY (acct_ref_id)
 			REFERENCES account_info(account_id)
 );
-
-ALTER TABLE account_info ADD COLUMN bank_id VARCHAR(20);
-
-ALTER TABLE account_info DROP CONSTRAINT account_id;
-
-DROP DATABASE user_accounts;
