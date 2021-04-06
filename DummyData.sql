e2e2611697a453cc881409494bb82e045aeed200
Mitchell <mitchellfrehr@gmail.com>
2021-04-05 20:34:07 -0400

    added dummy data to database

---
+++ DummyData.sql
@@ -0,0 +1,10 @@
+INSERT INTO account_info(first_name, 
+	last_name,
+	email,
+	primary_phone,
+	account_balance
+	)
+VALUES('mike','hawk','mikehawk@gmail.com',1318489503,15030.01),
+	('john','smith','johnsmith@yahoo.com',9784771203,8391.75),
+	('george','washington','woodteeth@government.gov',8290386512,218098.94);
+
